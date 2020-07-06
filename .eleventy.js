1990c9675406330cdb037b3899bdb6aadfdb9af9
Nish Bhat <nish@nmbhat.com>
2020-07-05 22:27:14 -0700

    ga

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
-  eleventyConfig.addPassthroughCopy('static')
+  eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('js')
   return {
     passthroughFileCopy: true
   }
