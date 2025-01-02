82affa51fc0f79ac50057262bf50141042ed3a60
Nish Bhat <nmbhat@gmail.com>
2025-01-02 10:08:17 +0800

    config

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
   eleventyConfig.addPassthroughCopy('assets')
   eleventyConfig.addPassthroughCopy('js')
+  eleventyConfig.addPassthroughCopy({ 'static/robots.txt': '/robots.txt' })
+  eleventyConfig.addPassthroughCopy({ 'static/feeds': '/feeds' })
   return {
     passthroughFileCopy: true
   }
